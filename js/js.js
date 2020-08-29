50b538bcc268c0e19475776a10fcc3c3b5a9cb3c
alex3959 <akmal_kamolov.tj57@bk.ru>
2020-08-29 19:59:32 +0500

    changed

--- js/js.js
+++ js/js.js
@@ -1,7 +1,24 @@
 $('.carousel').carousel({
-    interval: 3000
-  })
+  interval: 3000
+})
 
-$(function(){
+$(function () {
   new WOW().init();
+})
+
+let headerSection = document.querySelector('.header-section');
+let buttonTop = document.querySelector('.to-top');
+
+headerSection.addEventListener('mouseleave', function () {
+  if (buttonTop.classList.contains('hide')) {
+    buttonTop.classList.remove('hide');
+    buttonTop.classList.add('show');
+  }
+})
+
+headerSection.addEventListener('mouseover', function () {
+  if(buttonTop.classList.contains('show')){
+    buttonTop.classList.remove('show');
+    buttonTop.classList.add('hide');
+  }
 })
